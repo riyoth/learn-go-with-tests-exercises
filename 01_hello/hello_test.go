5ff052469e7ca0025a81d7f50961df3f68202e88
Pier-Olivier Clement <poclement@riyoth.info>
2024-04-03 11:38:51 -0400

    Implement french and refactore

--- 01_hello/hello_test.go
+++ 01_hello/hello_test.go
@@ -5,23 +5,45 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		some_name := "Chris"
-		got := Hello(some_name)
-		want := "Hello "+some_name
+		got := Hello(some_name, "")
+		want := "Hello " + some_name
 
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		the_default_value := "world"
-		got := Hello("")
-		want := "Hello "+ the_default_value
+		got := Hello("", "")
+		want := "Hello " + the_default_value
 
 		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		some_name := "Elodie"
+		got := Hello(some_name, "Spanish")
+		want := "Hola " + some_name
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		some_name := "Elodie"
+		got := Hello(some_name, "French")
+		want := "Bonjour " + some_name
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("explicit in english", func(t *testing.T){
+		some_name := "Elodie"
+		got := Hello(some_name, "English")
+		want := "Hello " + some_name
+
+		assertCorrectMessage(t, got, want)
+
 	})
 }
 
-func assertCorrectMessage(t testing.TB, got, want string){
+func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
-	if got != want{
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
--- 01_hello/main.go
+++ 01_hello/main.go
@@ -2,17 +2,40 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello "
+const (
+	englishHelloPrefix = "Hello "
+	english = "English"
+	spanishHelloPrefix = "Hola "
+	spanish = "Spanish"
+	frenchHelloPrefix = "Bonjour "
+	french = "French"
+)
+
 const defaultName = "world"
 
-func Hello(name string) string{
-	if name==""{
-		
-		return englishHelloPrefix+defaultName
+func Hello(name string, language string) string {
+	if name == "" {
+		name = defaultName
 	}
-	return englishHelloPrefix+name
+
+	return greetingPrefix(language)+ name
+}
+func greetingPrefix(language string) (prefix string){
+
+	switch language {
+		case english:
+			prefix = englishHelloPrefix
+		case french:
+			prefix = frenchHelloPrefix
+		case spanish:
+			prefix = spanishHelloPrefix
+		default:
+			prefix = englishHelloPrefix
+	}
+	return
+
 }
 
-func main(){
-	fmt.Println(Hello("chris"))
+func main() {
+	fmt.Println(Hello("chris", ""))
 }
