303759457ed9e65b08f88338de82ee8cd4dff2fa
Pier-Olivier Clement <poclement@riyoth.info>
2024-04-03 21:37:07 -0400

    Add shape name

--- 05_structs_methods_inteface/structs_test.go
+++ 05_structs_methods_inteface/structs_test.go
@@ -17,19 +17,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct{
+		name string
 		shape Shape
-		want float64
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 10.0, Height: 10.0},want: 100.0},
-		{shape: Circle{Radius: 10.0},want: 314.1592653589793},
-		{shape: Triangle{Base: 12.0,Height: 6.0}, want: 36.0},
+		{name: "Rectangle",shape: Rectangle{Width: 10.0, Height: 10.0},hasArea: 100.0},
+		{name: "Circle",shape: Circle{Radius: 10.0},hasArea: 314.1592653589793},
+		{name: "Triangle",shape: Triangle{Base: 12.0,Height: 6.0}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want{
-			t.Errorf("%#v - got %g want %g", tt.shape, got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T){
+			got := tt.shape.Area()
+			if got != tt.hasArea{
+				t.Errorf("%#v - got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
 	}
 }
