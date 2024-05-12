ee1673baa74ae1c7d66d3be4d6037daf33bb9f92
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-12 08:45:17 -0400

    start blogrenderer

--- 18_html_templates/blogrenderer/blogrenderer.go
+++ 18_html_templates/blogrenderer/blogrenderer.go
@@ -1,1 +1,12 @@
 package blogrenderer
+
+import "io"
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	return nil
+}
