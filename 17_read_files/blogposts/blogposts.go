4a693aacf8fbd55efea6ad8ac087b6e94b890e18
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-11 21:06:27 -0400

    Chapter 17

--- 17_read_files/blogposts/blogposts.go
+++ 17_read_files/blogposts/blogposts.go
@@ -1,6 +1,81 @@
 package blogposts
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"strings"
+)
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "tags: "
+)
+
 type Post struct {
-	Title, Description, Body string
-	Tags                     []string
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+func NewPostsFromFS(fileSsytem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSsytem, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+
+	for _, f := range dir {
+		post, err := getPost(fileSsytem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	post, err := newPost(postFile)
+	return post, nil
+}
+
+func readLine(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	title := readLine(scanner)[len(titleSeparator):]
+	description := readLine(scanner)[len(descriptionSeparator):]
+	tags := strings.Split(readLine(scanner)[len(tagsSeparator):], ", ")
+	body := readBody(scanner)
+
+	posts := Post{Title: title, Description: description, Tags: tags, Body: body}
+	return posts, nil
+
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	readLine(scanner)
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+
+	body := strings.TrimSuffix(buf.String(), "\n")
+	return body
 }
--- 17_read_files/blogposts/blogposts_test.go
+++ 17_read_files/blogposts/blogposts_test.go
@@ -1,21 +1,67 @@
 package blogposts_test
 
 import (
+	"errors"
+	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 
-	blogposts "github.com/riyoth/learn-go-with-tests-exercises/17_read_files/blogposts"
+	blogposts "github.com/riyoth/learn-go-with-tests-exercises/17_read_files/blogposts/blogposts"
 )
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+world`
+
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
 	fs := fstest.MapFS{
-		"hello_world.md":  {Data: []byte("hi")},
-		"hello_world2.md": {Data: []byte("hola")},
+		"hello_world.md":  {Data: []byte(firstBody)},
+		"hello_world2.md": {Data: []byte(secondBody)},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if len(posts) != len(fs) {
-		t.Errorf("go %d posts, wanted %d posts", len(posts), len(fs))
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1", Description: "Description 1", Tags: []string{"tdd", "go"}, Body: `Hello
+world`}
+
+	assertPost(t, got, want)
+}
+
+//func TestFailsToReadFS(t *testing.T) {
+//	_, err := blogposts.NewPostsFromFS(StubFailingFS{})
+//
+//	if err == nil {
+//		t.Fatal(err)
+//	}
+//}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, I always fail")
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
