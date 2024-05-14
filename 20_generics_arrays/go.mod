6eee208da242d9ba255eb197848cc4b7dc562b07
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-14 09:25:22 -0400

    Chapter 20 get started

---
+++ 20_generics_arrays/go.mod
@@ -0,0 +1,3 @@
+module generics_arrays
+
+go 1.22.2
---
+++ 20_generics_arrays/sum.go
@@ -0,0 +1,59 @@
+package main
+
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+func SumAll(numberToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numberToSum {
+		sums = append(sums, Sum(numbers))
+
+	}
+
+	return sums
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+
+	}
+	return Reduce(numbers, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
+}
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
+		}
+	}
+	return balance
+
+}
