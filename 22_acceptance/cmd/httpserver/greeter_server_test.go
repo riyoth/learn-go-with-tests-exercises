a7196d71b445bda9e1763ff7e3261d2088ff5f1e
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-17 18:56:25 -0400

    Organize file and module

--- 22_acceptance/cmd/httpserver/greeter_server_test.go
+++ 22_acceptance/cmd/httpserver/greeter_server_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/alecthomas/assert/v2"
-	go_specs_greet "github.com/quii/go-specs-greet"
+	"github.com/quii/go-specs-greet/adapters/httpserver"
 	"github.com/quii/go-specs-greet/specifications"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -38,6 +38,6 @@ func TestGreeterServer(t *testing.T) {
 	client := http.Client{
 		Timeout: 1 * time.Second,
 	}
-	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080", Client: &client}
+	driver := httpserver.Driver{BaseURL: "http://localhost:8080", Client: &client}
 	specifications.GreetSpecification(t, driver)
 }
