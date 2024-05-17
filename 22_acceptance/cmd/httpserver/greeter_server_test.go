4fb81f4b5e3760a58349b44c7ebdbf49ab9d229b
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-17 19:15:34 -0400

    extract the docker setup

--- 22_acceptance/cmd/httpserver/greeter_server_test.go
+++ 22_acceptance/cmd/httpserver/greeter_server_test.go
@@ -1,43 +1,26 @@
 package main_test
 
 import (
-	"context"
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
-	"github.com/alecthomas/assert/v2"
+	"github.com/quii/go-specs-greet/adapters"
 	"github.com/quii/go-specs-greet/adapters/httpserver"
 	"github.com/quii/go-specs-greet/specifications"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	ctx := context.Background()
+	var (
+		port           = "8080"
+		dockerFilePath = "./cmd/httpserver/Dockerfile"
+		baseURL        = fmt.Sprintf("http://localhost:%s", port)
+		driver         = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{
+			Timeout: 1 * time.Second,
+		}}
+	)
 
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context:       "../../.",
-			Dockerfile:    "./cmd/httpserver/Dockerfile",
-			PrintBuildLog: true,
-		},
-		ExposedPorts: []string{"8080:8080"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	}
-
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-
-	assert.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, container.Terminate(ctx))
-	})
-	client := http.Client{
-		Timeout: 1 * time.Second,
-	}
-	driver := httpserver.Driver{BaseURL: "http://localhost:8080", Client: &client}
+	adapters.StartDockerServer(t, port, dockerFilePath)
 	specifications.GreetSpecification(t, driver)
 }
--- 22_acceptance/domain/interactions/greet_test.go
+++ 22_acceptance/domain/interactions/greet_test.go
@@ -1,12 +1,12 @@
-package go_specs_greet_test
+package interactions_test
 
 import (
 	"testing"
 
-	go_specs_greet "github.com/quii/go-specs-greet"
+	"github.com/quii/go-specs-greet/domain/interactions"
 	"github.com/quii/go-specs-greet/specifications"
 )
 
 func TestGreet(t *testing.T) {
-	specifications.GreetSpecification(t, specifications.GreetAdapter(go_specs_greet.Greet))
+	specifications.GreetSpecification(t, specifications.GreetAdapter(interactions.Greet))
 }
