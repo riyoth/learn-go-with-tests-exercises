9e1e61834710b0c96177232f38462ea4277b6571
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-20 19:59:29 -0400

    Make the test pass

--- 25_make_an_app/main.go
+++ 25_make_an_app/main.go
@@ -11,6 +11,8 @@ func (i *inMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
 }
 
+func (i *inMemoryPlayerStore) RecordWin(name string) {}
+
 func main() {
 	server := &PlayerServer{&inMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
--- 25_make_an_app/server.go
+++ 25_make_an_app/server.go
@@ -12,6 +12,7 @@ type PlayerServer struct {
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -36,5 +37,6 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	p.store.RecordWin("bob")
 	w.WriteHeader(http.StatusAccepted)
 }
