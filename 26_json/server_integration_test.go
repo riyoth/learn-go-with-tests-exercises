5d9d64b59dd895b234f6bf20c5731f8d93fd7456
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-21 13:51:22 -0400

    Add integration test

--- 26_json/server_integration_test.go
+++ 26_json/server_integration_test.go
@@ -14,10 +14,24 @@ func TestRecordingWindsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "3")
+	})
 
-	assertResponseBody(t, response.Body.String(), "3")
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+		assertStatus(t, response.Code, http.StatusOK)
+
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{"Pepper", 1},
+		}
+
+		assertLeague(t, got, want)
+	})
 }
--- 26_json/server_test.go
+++ 26_json/server_test.go
@@ -112,9 +112,7 @@ func TestLeague(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
 
-		if response.Result().Header.Get("content-type") != "application/json" {
-			t.Errorf("response ddi not have content-type of application/json, got '%v'", response.Result().Header)
-		}
+		assertContentType(t, response, "application/json")
 
 	})
 }
@@ -164,5 +162,11 @@ func assertLeague(t testing.TB, got, want []Player) {
 	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
+	}
+}
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did have content-type of %s, got %v", want, response.Result().Header)
 	}
 }
