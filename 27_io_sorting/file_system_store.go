2e47c97d2b00641eefddfabce1ad85fd42d5b36c
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-22 16:56:40 -0400

    Clean up filesystemplayerstore constructor

--- 27_io_sorting/file_system_store.go
+++ 27_io_sorting/file_system_store.go
@@ -13,16 +13,9 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, io.SeekStart)
-
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 	if err != nil {
-		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
-
-	}
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, io.SeekStart)
+		return nil, fmt.Errorf("problem initialising player db file  %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -35,6 +28,22 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, io.SeekStart)
+
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
+
+	}
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, io.SeekStart)
+	}
+
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
