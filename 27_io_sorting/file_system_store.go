460439954d23544901f41c7ae479c5a62e23652e
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-22 13:43:43 -0400

    Add get player score to persistent storage

--- 27_io_sorting/file_system_store.go
+++ 27_io_sorting/file_system_store.go
@@ -5,10 +5,24 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
 }
