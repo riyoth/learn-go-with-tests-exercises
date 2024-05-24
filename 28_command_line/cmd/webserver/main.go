0393d2c963c5e19b4c9afe86d516236e671083b4
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-24 10:18:25 -0400

    Refactor

--- 28_command_line/cmd/webserver/main.go
+++ 28_command_line/cmd/webserver/main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"github.com/riyoth/learn-go-with-tests-exercises/28_command_line"
 	"log"
 	"net/http"
-	"os"
+
+	poker "github.com/riyoth/learn-go-with-tests-exercises/28_command_line"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("rpoblem creating file system player store, %v", err)
-	}
+	defer close()
+
 	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
