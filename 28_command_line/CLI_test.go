cd50366afc7b4394d9c7dfa0f5e3cdeab5a1adcd
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-24 10:07:44 -0400

    CLI working

--- 28_command_line/CLI_test.go
+++ 28_command_line/CLI_test.go
@@ -1,40 +1,29 @@
-package poker
+package poker_test
 
 import (
 	"strings"
 	"testing"
+
+	poker "github.com/riyoth/learn-go-with-tests-exercises/28_command_line"
 )
 
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
-		playerStore := &StubPlayerStore{}
+		playerStore := &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := &poker.CLI{playerStore, in}
 		cli.PlayPoker()
 
-		assertPlayerWin(t, playerStore, "Chris")
+		AssertPlayerWin(t, playerStore, "Chris")
 	})
 	t.Run("record Cleo win from user input", func(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
-		playerStore := &StubPlayerStore{}
+		playerStore := &poker.StubPlayerStore{}
 
-		cli := &CLI{playerStore, in}
+		cli := &poker.CLI{playerStore, in}
 		cli.PlayPoker()
 
-		assertPlayerWin(t, playerStore, "Cleo")
+		AssertPlayerWin(t, playerStore, "Cleo")
 	})
-}
-
-func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
-	t.Helper()
-
-	if len(store.winCalls) != 1 {
-		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-	}
-
-	if store.winCalls[0] != winner {
-		t.Errorf("didn't record correct winner, got %q,  want %q", store.winCalls[0], winner)
-
-	}
 }
--- 28_command_line/server_test.go
+++ 28_command_line/server_test.go
@@ -10,24 +10,6 @@ import (
 	"testing"
 )
 
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
 func TestGetPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
