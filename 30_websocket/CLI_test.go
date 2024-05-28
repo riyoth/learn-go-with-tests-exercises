9e40c45c361de4d8dc0468fc911c19a1a6bb4ea3
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-28 16:23:13 -0400

    get started with websocket chapter

---
+++ 30_websocket/CLI_test.go
@@ -0,0 +1,104 @@
+package poker_test
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	poker "github.com/riyoth/learn-go-with-tests-exercises/28_command_line"
+)
+
+var dummyBlindAlerter = &poker.SpyBlindAlerter{}
+var dummyPlayerStore = &poker.StubPlayerStore{}
+var dummyStdIn = &bytes.Buffer{}
+var dummyStdOut = &bytes.Buffer{}
+
+type GameSpy struct {
+	StartedWith  int
+	StartCalled  bool
+	FinishedWith string
+}
+
+func (p *GameSpy) Start(numberOfPlayers int) {
+	p.StartCalled = true
+	p.StartedWith = numberOfPlayers
+}
+
+func (p *GameSpy) Finish(winner string) {
+	p.FinishedWith = winner
+}
+
+func userSends(messages ...string) io.Reader {
+	return strings.NewReader(strings.Join(messages, "\n"))
+}
+
+func TestCLI(t *testing.T) {
+	t.Run("it prompts the user to enter the number of player and start the game", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		in := userSends("7")
+		game := &GameSpy{}
+
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+
+		assertMessagesSentToUser(t, stdout, poker.PlayerPrompt)
+		assertGameStartedWith(t, game, 7)
+	})
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := userSends("1", "Chris wins")
+		game := &GameSpy{}
+
+		cli := poker.NewCLI(in, dummyStdOut, game)
+		cli.PlayPoker()
+
+		assertGameFinishWith(t, game, "Chris")
+	})
+	t.Run("record Cleo win from user input", func(t *testing.T) {
+		in := userSends("1", "Cleo wins")
+		game := &GameSpy{}
+
+		cli := poker.NewCLI(in, dummyStdOut, game)
+		cli.PlayPoker()
+
+		assertGameFinishWith(t, game, "Cleo")
+	})
+	t.Run("it prints an error when a non numeric value is entered and does not start the game", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		in := userSends("Pies")
+		game := &GameSpy{}
+
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+
+		if game.StartCalled {
+			t.Errorf("Game should not have started")
+		}
+
+		wantPrompt := poker.PlayerPrompt + poker.BadPlayerInputErrMsg
+
+		assertMessagesSentToUser(t, stdout, wantPrompt)
+	})
+}
+
+func assertGameStartedWith(t testing.TB, game *GameSpy, want int) {
+	t.Helper()
+	if game.StartedWith != want {
+		t.Errorf("wanted Start called with %d but got %d", want, game.StartedWith)
+	}
+}
+func assertGameFinishWith(t testing.TB, game *GameSpy, want string) {
+	t.Helper()
+	if game.FinishedWith != want {
+		t.Errorf("wanted Finish called with %v but got %v", want, game.FinishedWith)
+	}
+}
+func assertMessagesSentToUser(t testing.TB, stdout *bytes.Buffer, messages ...string) {
+	t.Helper()
+	want := strings.Join(messages, "")
+	got := stdout.String()
+
+	if got != want {
+		t.Errorf("got %q sent to stdout but expected %+v", got, messages)
+	}
+}
---
+++ 30_websocket/testing.go
@@ -0,0 +1,66 @@
+package poker
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+	league   []Player
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
+func (s *StubPlayerStore) GetLeague() League {
+	return s.league
+}
+
+func AssertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("didn't record correct winner, got %q,  want %q", store.winCalls[0], winner)
+
+	}
+}
+
+type ScheduledAlert struct {
+	At     time.Duration
+	Amount int
+}
+
+func (s ScheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.Amount, s.At)
+}
+
+func AssertScheduledAlert(t testing.TB, got ScheduledAlert, want ScheduledAlert) {
+	if got.Amount != want.Amount {
+		t.Errorf("got amount %d, want %d", got.Amount, want.Amount)
+	}
+
+	if got.At != want.At {
+		t.Errorf("got scheduled time of %v, want %v", got.At, want.At)
+	}
+}
+
+type SpyBlindAlerter struct {
+	Alerts []ScheduledAlert
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.Alerts = append(s.Alerts, ScheduledAlert{duration, amount})
+}
