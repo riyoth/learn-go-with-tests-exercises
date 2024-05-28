46d9f7b047a9b9136c1e80ce89f1f1ee5b698a1d
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-28 10:42:40 -0400

    point in time

--- 29_time/CLI.go
+++ 29_time/CLI.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -28,21 +29,27 @@ func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter
 
 func (cli *CLI) PlayPoker() {
 	fmt.Fprintf(cli.out, PlayerPrompt)
-	cli.scheduleBlindAlerts()
+	numberOfPlayers, _ := strconv.Atoi(cli.readLine())
+	cli.scheduleBlindAlerts(numberOfPlayers)
 	reader := cli.in
 	reader.Scan()
 	cli.playerStore.RecordWin(extractWinner(reader.Text()))
 }
 
-func extractWinner(userInput string) string {
-	return strings.Replace(userInput, " wins", "", 1)
-}
-
-func (cli *CLI) scheduleBlindAlerts() {
+func (cli *CLI) scheduleBlindAlerts(numberOfPlayers int) {
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, blind)
 		blindTime = blindTime + 10*time.Minute
 	}
+}
+
+func (cli *CLI) readLine() string {
+	cli.in.Scan()
+	return cli.in.Text()
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
