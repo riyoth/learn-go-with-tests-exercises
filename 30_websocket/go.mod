8d80037689a495e33227d082d0fa6d9b7a43555b
Pier-Olivier Clement <poclement@riyoth.info>
2024-05-28 17:23:20 -0400

    Add websocket

--- 30_websocket/go.mod
+++ 30_websocket/go.mod
@@ -1,3 +1,8 @@
 module github.com/riyoth/learn-go-with-tests-exercises/28_command_line
 
 go 1.22.3
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- 30_websocket/server_test.go
+++ 30_websocket/server_test.go
@@ -7,8 +7,46 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+func TestGame(t *testing.T) {
+	t.Run("GET /game return 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request := newGameRequest()
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response, http.StatusOK)
+	})
+	t.Run("when we get a message over a websocket it is a winner of a game", func(t *testing.T) {
+		store := &StubPlayerStore{}
+		winner := "Ruth"
+		server := httptest.NewServer(NewPlayerServer(store))
+		defer server.Close()
+
+		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
+
+		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+		if err != nil {
+			t.Fatalf("could not open a ws connection %v", err)
+		}
+		defer ws.Close()
+
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(winner)); err != nil {
+			t.Fatalf("could not send message over ws connection %v", err)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+		AssertPlayerWin(t, store, winner)
+	})
+}
 
 func TestGetPlayers(t *testing.T) {
 	store := StubPlayerStore{
@@ -27,7 +65,7 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		assertStatus(t, response.Code, 200)
+		assertStatus(t, response, 200)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 	t.Run("return Floyd's score", func(t *testing.T) {
@@ -36,7 +74,7 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		assertStatus(t, response.Code, 200)
+		assertStatus(t, response, 200)
 		assertResponseBody(t, response.Body.String(), "10")
 	})
 	t.Run("return 404 on missing players", func(t *testing.T) {
@@ -45,7 +83,7 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		assertStatus(t, response.Code, 404)
+		assertStatus(t, response, 404)
 	})
 }
 
@@ -64,7 +102,7 @@ func TestStoreWins(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		assertStatus(t, response.Code, http.StatusAccepted)
+		assertStatus(t, response, http.StatusAccepted)
 		if len(store.winCalls) != 1 {
 			t.Errorf("got %d calls to  RecordWin wnat %d", len(store.winCalls), 1)
 		}
@@ -91,7 +129,7 @@ func TestLeague(t *testing.T) {
 
 		got := getLeagueFromResponse(t, response.Body)
 
-		assertStatus(t, response.Code, http.StatusOK)
+		assertStatus(t, response, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
 
 		assertContentType(t, response, "application/json")
@@ -114,6 +152,11 @@ func newPostWinRequest(name string) *http.Request {
 	return req
 }
 
+func newGameRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/game", nil)
+	return req
+}
+
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
 
@@ -133,10 +176,10 @@ func assertResponseBody(t testing.TB, got, want string) {
 	}
 }
 
-func assertStatus(t testing.TB, got, want int) {
+func assertStatus(t testing.TB, got *httptest.ResponseRecorder, want int) {
 	t.Helper()
-	if got != want {
-		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	if got.Code != want {
+		t.Errorf("did not get correct status, got %d, want %d", got.Code, want)
 	}
 }
 
